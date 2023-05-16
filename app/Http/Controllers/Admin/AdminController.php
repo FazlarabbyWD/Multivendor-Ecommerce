1bccdd4813bd5cc6719fc07f4102b0b746499736
FAZLA RABBY <fazla.rabbyWD@gmail.com>
2023-05-16 06:55:29 +0600

    profile updated

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use app\Models\User;
 
 class AdminController extends Controller
 {
@@ -26,6 +27,27 @@ class AdminController extends Controller
         return redirect('admin/login');
     }
     function profile(){
-        return view('admin.profile');
+        $adminInfo = User::find(Auth::user()->id);
+        return view('admin.profile',compact(('adminInfo')));
     }
+    function updateProfile (Request $request){
+        $adminData = User::find(Auth::user()->id);
+        $adminData->name = $request->name;
+        $adminData->userName= $request->userName;
+        $adminData->email= $request->email;
+        $adminData->phone= $request->phone;
+        $adminData->address= $request->address;
+
+        if($request->image){
+            $image = $request->file('image');
+            $customName = rand().'.'.$image->getClientOriginalExtension();
+            @unlink(public_path('uploads/admin/'. $adminData->profile_pic));
+            $image->move(public_path('uploads/admin/'),$customName);
+            $adminData->profile_pic = $customName;
+
+        }
+        $adminData->update();
+        return back();
+    }
+
 }
